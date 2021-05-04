6b1a8e10347f204644c0c3e7e07c3a381cc64a7d
SaraAli22 <sarasalem48@yahoo.com>
2021-05-04 04:25:01 +0300

    Solve P5

--- app.js
+++ app.js
@@ -104,11 +104,14 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function multiplyArray(multArr) {
+  let multi3=multiply(2,3)[0];
+  let multi4=multiply(multi3,4)[0];
+  return [multi4,'The numbers 2,3,4 have a product of 24.'];
   //eslint-disable-line
 }
 
 // Here is the test for multiplyArray(); uncomment it to run it
-// testMultiplyArray(testArray);
+ testMultiplyArray(testArray);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop.
 
